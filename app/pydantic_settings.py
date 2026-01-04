3e107a3ed98a2503d4e6e39c4620467248fbe7f8
dhwanilraval127-stack <dhwanil.raval127@gmail.com>
2026-01-04 23:11:45 +0530

    Delete app directory

--- app/pydantic_settings.py
+++
@@ -1,9 +0,0 @@
-"""
-Pydantic Settings for configuration
-"""
-try:
-    from pydantic_settings import BaseSettings
-except ImportError:
-    from pydantic import BaseSettings
-
-__all__ = ['BaseSettings']
--- app/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Utility functions for DhartiSetu
-"""
-from .helpers import *
